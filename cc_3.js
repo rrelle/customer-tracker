ac7019e4bdd2e40a9fed49c5c6c65e77ba93b494
Rrelle <rrelleferguson@usf.edu>
2026-02-08 21:25:11 -0500

    went make to make purchases an array string instead of string

--- cc_3.js
+++ cc_3.js
@@ -1,22 +1,22 @@
 const customers = [
-  { id: 1, name: "Alice Johnson", email: "alice@example.com", purchases: "Lipgloss" },
-  { id: 2, name: "Bob Smith", email: "bob@example.com", purchases: "Keyboard" },
-  { id: 3, name: "Charlie Brown", email: "charlie@example.com", purchases: "Mouse" }
+  { id: 1, name: "Alice Johnson", email: "alice@example.com", purchases: ["Lipgloss"] },
+  { id: 2, name: "Bob Smith", email: "bob@example.com", purchases: ["Keyboard"] },
+  { id: 3, name: "Charlie Brown", email: "charlie@example.com", purchases: ["Mouse"] }
 ];
 
-customers.push({ id: 4, name: "David Wilson", email: "david@example.com", purchases: "Headphones" });
+customers.push({ id: 4, name: "David Wilson", email: "david@example.com", purchases: ["Headphones"] });
 customers.shift();
 
 console.log(customers);
 customers[1].email = "charlie.updated@example.com";
 console.log(customers);
 
-
-customers[1].purchases.push = "Monitor";
-console.log(customers);
+customers[0].purchases.push("Lipstick");
+console.log(customers[0].purchases);
 
 customers.forEach(customer => {
   console.log(`Customer Name: ${customer.name}, Email: ${customer.email}, Purchases: ${customer.purchases}`);
 });
 console.log(customers.length);
+
 
